fafec60bc4295a0934e7cc4718232f8ef5cc9536
Anil <anil.narra@trinity-health.org>
2018-12-20 16:10:10 -0500

    Fixing user name

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,14 +8,14 @@ class UserHeader extends React.Component {
 		if(!user) {
 			return <div className="header">Anil</div>;
 		}
-		return <div className="header">{user.username}</div>;
+		return <div className="header">{user.name}</div>;
 
 
 	}
 };
 
 const mapStateToProps = (state, ownProps) => {
-	return {users: state.users.find(user => user.id === ownProps.userId)};
+	return {user: state.users.find(user => user.id === ownProps.userId)};
 };
 
 export default connect(mapStateToProps)(UserHeader);

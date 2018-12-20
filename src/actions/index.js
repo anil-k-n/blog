97798cb8791ee93396e5aaac696eb90d3f09e3a1
Anil <anil.narra@trinity-health.org>
2018-12-20 08:58:06 -0500

    First Commit

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+		const reponse = await jsonPlaceholder.get('/posts');
+		dispatch({type: 'FETCH_POSTS', payload: reponse.data});
+};
+
+
+export const fetchUser = (id) => async dispatch => {
+		const reponse = await jsonPlaceholder.get(`/users/${id}`);
+		dispatch({type: 'FETCH_USER', payload: reponse.data});
+};
+
